aaecc167e084a270c4f38ab89ad595cf14f8a200
Shayan Ahmad <shayanahmad235@gmail.com>
2024-08-06 21:31:10 +0500

    after login success message show

---
+++ success-message.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>User Login</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+	<div class="phppot-container text-center">
+		<div class="success inline-block">You are successfully authenticated!</div>
+	</div>
+</body>
+</html>
